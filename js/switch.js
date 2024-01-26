c5d0b9c4cbcbd561d527a3e63114c5029a734e77
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2024-01-26 19:17:29 +0100

    Fix dark switch

--- js/switch.js
+++ js/switch.js
@@ -1,101 +1,79 @@
-/**
- *  Light Switch @version v0.1.4
- */
-
 (function () {
-  let lightSwitch = document.getElementById('lightSwitch');
+  let lightSwitch = document.getElementById("lightSwitch");
   if (!lightSwitch) {
     return;
   }
 
-  /**
-   * @function darkmode
-   * @summary: changes the theme to 'dark mode' and save settings to local stroage.
-   * Basically, replaces/toggles every CSS class that has '-light' class with '-dark'
-   */
   function darkMode() {
-    document.querySelectorAll('.bg-light').forEach((element) => {
-      element.className = element.className.replace(/-light/g, '-dark');
+    document.querySelectorAll(".bg-light").forEach((element) => {
+      element.className = element.className.replace(/-light/g, "-dark");
     });
 
-    document.body.classList.add('bg-dark');
-    document.body.innerText.add('bg-dark');
+    document.body.classList.add("bg-dark");
+    document.body.classList.remove("bg-light");
 
-    if (document.body.classList.contains('text-dark')) {
-      document.body.classList.replace('text-dark', 'text-light');
-    } else {
-      document.body.classList.add('text-light');
-    }
+    // Set text color to white
+    document.body.classList.remove("text-dark");
+    document.body.classList.add("text-light");
 
     // Tables
-    var tables = document.querySelectorAll('table');
+    var tables = document.querySelectorAll("table");
     for (var i = 0; i < tables.length; i++) {
-      // add table-dark class to each table
-      tables[i].classList.add('table-dark');
+      tables[i].classList.add("table-dark");
     }
 
-    // set light switch input to true
     if (!lightSwitch.checked) {
       lightSwitch.checked = true;
     }
-    localStorage.setItem('lightSwitch', 'dark');
+    localStorage.setItem("lightSwitch", "dark");
   }
 
-  /**
-   * @function lightmode
-   * @summary: changes the theme to 'light mode' and save settings to local stroage.
-   */
   function lightMode() {
-    document.querySelectorAll('.bg-dark').forEach((element) => {
-      element.className = element.className.replace(/-dark/g, '-light');
+    document.querySelectorAll(".bg-dark").forEach((element) => {
+      element.className = element.className.replace(/-dark/g, "-light");
     });
 
-    document.body.classList.add('bg-light');
+    document.body.classList.add("bg-light");
+    document.body.classList.remove("bg-dark");
 
-    if (document.body.classList.contains('text-light')) {
-      document.body.classList.replace('text-light', 'text-dark');
-    } else {
-      document.body.classList.add('text-dark');
-    }
+    // Set text color to black
+    document.body.classList.remove("text-light");
+    document.body.classList.add("text-dark");
 
     // Tables
-    var tables = document.querySelectorAll('table');
+    var tables = document.querySelectorAll("table");
     for (var i = 0; i < tables.length; i++) {
-      if (tables[i].classList.contains('table-dark')) {
-        tables[i].classList.remove('table-dark');
+      if (tables[i].classList.contains("table-dark")) {
+        tables[i].classList.remove("table-dark");
       }
     }
 
     if (lightSwitch.checked) {
       lightSwitch.checked = false;
     }
-    localStorage.setItem('lightSwitch', 'light');
+    localStorage.setItem("lightSwitch", "light");
   }
 
   function hideLightSVG() {
-    const svg = document.getElementById('hide-light');
-    svg.style.display = 'none';
+    const svg = document.getElementById("hide-light");
+    svg.style.display = "none";
   }
 
   function showLightSVG() {
-    const svg = document.getElementById('hide-light');
-    svg.style.display = 'block';
+    const svg = document.getElementById("hide-light");
+    svg.style.display = "block";
   }
 
   function hideDarkSVG() {
-    const svg = document.getElementById('hide-dark');
-    svg.style.display = 'none';
+    const svg = document.getElementById("hide-dark");
+    svg.style.display = "none";
   }
 
   function showDarkSVG() {
-    const svg = document.getElementById('hide-dark');
-    svg.style.display = 'block';
+    const svg = document.getElementById("hide-dark");
+    svg.style.display = "block";
   }
 
-  /**
-   * @function onToggleMode
-   * @summary: the event handler attached to the switch. calling @darkMode or @lightMode depending on the checked state.
-   */
   function onToggleMode() {
     if (lightSwitch.checked) {
       darkMode();
@@ -108,29 +86,25 @@
     }
   }
 
-  /**
-   * @function getSystemDefaultTheme
-   * @summary: get system default theme by media query
-   */
   function getSystemDefaultTheme() {
-    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
     if (darkThemeMq.matches) {
-      return 'dark';
+      return "dark";
     }
-    return 'light';
+    return "light";
   }
 
   function setup() {
-    var settings = localStorage.getItem('lightSwitch');
+    var settings = localStorage.getItem("lightSwitch");
     if (settings == null) {
       settings = getSystemDefaultTheme();
     }
 
-    if (settings == 'dark') {
+    if (settings == "dark") {
       lightSwitch.checked = true;
     }
 
-    lightSwitch.addEventListener('change', onToggleMode);
+    lightSwitch.addEventListener("change", onToggleMode);
     onToggleMode();
   }
 
